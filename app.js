a848747be7f6f4ebe8eaaa70a843f6aa201db224
unknown <cocarundsn@gmail.com>
2020-08-04 20:05:08 +0530

    hey commited
    Signed-off-by: unknown <cocarundsn@gmail.com>

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const navlink = document.querySelector(".navlinks");
 const links = document.querySelectorAll(".navlinks li")
 burger.addEventListener("click", () => {
     navlink.classList.toggle("navopen");
+    links.forEach(link =>{
+        link.classList.toggle("fade");
+    });
     });
 
