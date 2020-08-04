7bb10454bbdb36ed75fb65416b5f9738d0d8a0dc
Arun Senthilnathan (TCS) <Arun_Senthilnathan@belk.com>
2020-08-04 10:06:23 -0400

    added opacity

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const burger = document.querySelector("#bur");
 const navlink = document.querySelector(".navlinks");
+const links = document.querySelectorAll(".navlinks li")
 burger.addEventListener("click", () => {
     navlink.classList.toggle("navopen");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
 });
 
