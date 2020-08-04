ea5f76aa36e2ba54547ed02d88dd9d9ea891dd0a
Arun Senthilnathan (TCS) <Arun_Senthilnathan@belk.com>
2020-08-04 10:22:02 -0400

    comits

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const links = document.querySelectorAll(".navlinks li")
 burger.addEventListener("click", () => {
     navlink.classList.toggle("navopen");
     links.forEach(link => {
-        link.classList.toggle("fades");
+        link.classList.toggle("fade");
     });
 });
 
