e0a2036882c150a6ea582b5adf2d1095689a5fb4
unknown <cocarundsn@gmail.com>
2020-08-05 12:33:38 +0530

    animate nav
    Signed-off-by: unknown <cocarundsn@gmail.com>

--- script/app.js
+++ script/app.js
@@ -1,12 +1,24 @@
 const burger = document.querySelector("#bur");
 const navlink = document.querySelector(".navlinks");
 const links = document.querySelectorAll(".navlinks li")
+const line = document.querySelectorAll(".burger div");
 burger.addEventListener("click", () => {
     navlink.classList.toggle("navopen");
+    console.log(line.classList);
     console.log(navlink.className);
     links.forEach(link => {
         link.classList.toggle("fade");
-        console.log(link.classList);
     });
+    line.forEach((lines,index )=> {
+        if (index ==0) {
+            lines.classList.toggle("burgerfade");
+        }
+        else if(index ==1){
+            lines.classList.toggle("burgerfade1");
+        }
+     else if (index == 2 ){
+            lines.classList.toggle("burgerfade2");
+        };
     });
+});
 
